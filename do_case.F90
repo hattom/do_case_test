895fe1c98291b95aa89deb7dd4d1b112e2a5f650
Thomas Hayward-Schneider <thomas.hayward@ipp.mpg.de>
2020-12-26 19:50:21 +0100

    Mem layout + timings
    * Add option to switch between row major and column major memory access
    * Add no-op loop
    * Add timings.txt

--- do_case.F90
+++ do_case.F90
@@ -1,9 +1,20 @@
 #define nvars 10
 
 #ifdef versionA
-#define ncols 10
+#define ncols nvars
 #else
 #define ncols 1
+#endif
+
+#ifndef ROW_MAJ
+#if !defined(COL_MAJ)
+#define COL_MAJ
+#endif
+#define ELEM(x,y) x,y
+#define NP_DIM 2
+#else
+#define ELEM(x,y) y,x
+#define NP_DIM 1
 #endif
 
 program test
@@ -16,7 +27,10 @@ program test
   
   i1=1; i2=2; i3=3; i4=4; i5=5; i6=6; i7=7; i8=8; i9=9; i10=10
 
-  allocate(array(ncols,np))
+  allocate(array(ELEM(ncols,np)))
+#ifdef DEBUG
+  print *, shape(array)
+#endif
 
   do i=1,n
     call test_sub(array, mod(i, nvars)+1, i1,i2,i3,i4,i5,i6,i7,i8,i9,i10)
@@ -33,88 +47,88 @@ contains
     integer, intent(in) :: iter, i1,i2,i3,i4,i5,i6,i7,i8,i9,i10
     integer :: i, imax
 
-    imax = ubound(arr, 2)
+    imax = ubound(arr, NP_DIM)
     !$omp parallel do schedule(static)
     do i=1, imax
       ! do lots of stuff, some of which the below expressions should depend on
 #ifdef versionA
-      arr(1,i) = real(i, kind=REAL64)
-      arr(2,i) = real(2*i, kind=REAL64)
-      arr(3,i) = real(3*i, kind=REAL64)
-      arr(4,i) = real(4*i, kind=REAL64)
-      arr(5,i) = real(5*i, kind=REAL64)
-      arr(6,i) = real(6*i, kind=REAL64)
-      arr(7,i) = real(7*i, kind=REAL64)
-      arr(8,i) = real(8*i, kind=REAL64)
-      arr(9,i) = real(9*i, kind=REAL64)
-      arr(10,i) = real(10*i, kind=REAL64)
+      arr(ELEM(1,i)) = real(i, kind=REAL64)
+      arr(ELEM(2,i)) = real(2*i, kind=REAL64)
+      arr(ELEM(3,i)) = real(3*i, kind=REAL64)
+      arr(ELEM(4,i)) = real(4*i, kind=REAL64)
+      arr(ELEM(5,i)) = real(5*i, kind=REAL64)
+      arr(ELEM(6,i)) = real(6*i, kind=REAL64)
+      arr(ELEM(7,i)) = real(7*i, kind=REAL64)
+      arr(ELEM(8,i)) = real(8*i, kind=REAL64)
+      arr(ELEM(9,i)) = real(9*i, kind=REAL64)
+      arr(ELEM(10,i)) = real(10*i, kind=REAL64)
 #elif defined(versionB)
       select case(iter)
       case(1)
-        arr(1,i) = real(i, kind=REAL64)
+        arr(ELEM(1,i)) = real(i, kind=REAL64)
       case(2)
-        arr(1,i) = real(2*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(2*i, kind=REAL64)
       case(3)
-        arr(1,i) = real(3*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(3*i, kind=REAL64)
       case(4)
-        arr(1,i) = real(4*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(4*i, kind=REAL64)
       case(5)
-        arr(1,i) = real(5*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(5*i, kind=REAL64)
       case(6)
-        arr(1,i) = real(6*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(6*i, kind=REAL64)
       case(7)
-        arr(1,i) = real(7*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(7*i, kind=REAL64)
       case(8)
-        arr(1,i) = real(8*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(8*i, kind=REAL64)
       case(9)
-        arr(1,i) = real(9*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(9*i, kind=REAL64)
       case(10)
-        arr(1,i) = real(10*i, kind=REAL64)
+        arr(ELEM(1,i)) = real(10*i, kind=REAL64)
       end select
 #elif defined(versionC)
-      if(iter==1)  arr(1,i) = real(i, kind=REAL64)
-      if(iter==2)  arr(1,i) = real(2*i, kind=REAL64)
-      if(iter==3)  arr(1,i) = real(3*i, kind=REAL64)
-      if(iter==4)  arr(1,i) = real(4*i, kind=REAL64)
-      if(iter==5)  arr(1,i) = real(5*i, kind=REAL64)
-      if(iter==6)  arr(1,i) = real(6*i, kind=REAL64)
-      if(iter==7)  arr(1,i) = real(7*i, kind=REAL64)
-      if(iter==8)  arr(1,i) = real(8*i, kind=REAL64)
-      if(iter==9)  arr(1,i) = real(9*i, kind=REAL64)
-      if(iter==10) arr(1,i) = real(10*i, kind=REAL64)
+      if(iter==1)  arr(ELEM(1,i)) = real(i, kind=REAL64)
+      if(iter==2)  arr(ELEM(1,i)) = real(2*i, kind=REAL64)
+      if(iter==3)  arr(ELEM(1,i)) = real(3*i, kind=REAL64)
+      if(iter==4)  arr(ELEM(1,i)) = real(4*i, kind=REAL64)
+      if(iter==5)  arr(ELEM(1,i)) = real(5*i, kind=REAL64)
+      if(iter==6)  arr(ELEM(1,i)) = real(6*i, kind=REAL64)
+      if(iter==7)  arr(ELEM(1,i)) = real(7*i, kind=REAL64)
+      if(iter==8)  arr(ELEM(1,i)) = real(8*i, kind=REAL64)
+      if(iter==9)  arr(ELEM(1,i)) = real(9*i, kind=REAL64)
+      if(iter==10) arr(ELEM(1,i)) = real(10*i, kind=REAL64)
 #elif defined(versionD)
       if(iter==1) then
-         arr(1,i) = real(i, kind=REAL64)
+         arr(ELEM(1,i)) = real(i, kind=REAL64)
       elseif(iter==2) then
-         arr(1,i) = real(2*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(2*i, kind=REAL64)
       elseif(iter==3) then
-         arr(1,i) = real(3*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(3*i, kind=REAL64)
       elseif(iter==4) then
-         arr(1,i) = real(4*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(4*i, kind=REAL64)
       elseif(iter==5) then
-         arr(1,i) = real(5*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(5*i, kind=REAL64)
       elseif(iter==6) then
-         arr(1,i) = real(6*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(6*i, kind=REAL64)
       elseif(iter==7) then
-         arr(1,i) = real(7*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(7*i, kind=REAL64)
       elseif(iter==8) then
-         arr(1,i) = real(8*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(8*i, kind=REAL64)
       elseif(iter==9) then
-         arr(1,i) = real(9*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(9*i, kind=REAL64)
       elseif(iter==10) then
-         arr(1,i) = real(10*i, kind=REAL64)
+         arr(ELEM(1,i)) = real(10*i, kind=REAL64)
       endif
-#else
-      if(iter==i1)  arr(1,i) = real(i, kind=REAL64)
-      if(iter==i2)  arr(1,i) = real(2*i, kind=REAL64)
-      if(iter==i3)  arr(1,i) = real(3*i, kind=REAL64)
-      if(iter==i4)  arr(1,i) = real(4*i, kind=REAL64)
-      if(iter==i5)  arr(1,i) = real(5*i, kind=REAL64)
-      if(iter==i6)  arr(1,i) = real(6*i, kind=REAL64)
-      if(iter==i7)  arr(1,i) = real(7*i, kind=REAL64)
-      if(iter==i8)  arr(1,i) = real(8*i, kind=REAL64)
-      if(iter==i9)  arr(1,i) = real(9*i, kind=REAL64)
-      if(iter==i10) arr(1,i) = real(10*i, kind=REAL64)
+#elif defined(versionE)
+      if(iter==i1)  arr(ELEM(1,i)) = real(i, kind=REAL64)
+      if(iter==i2)  arr(ELEM(1,i)) = real(2*i, kind=REAL64)
+      if(iter==i3)  arr(ELEM(1,i)) = real(3*i, kind=REAL64)
+      if(iter==i4)  arr(ELEM(1,i)) = real(4*i, kind=REAL64)
+      if(iter==i5)  arr(ELEM(1,i)) = real(5*i, kind=REAL64)
+      if(iter==i6)  arr(ELEM(1,i)) = real(6*i, kind=REAL64)
+      if(iter==i7)  arr(ELEM(1,i)) = real(7*i, kind=REAL64)
+      if(iter==i8)  arr(ELEM(1,i)) = real(8*i, kind=REAL64)
+      if(iter==i9)  arr(ELEM(1,i)) = real(9*i, kind=REAL64)
+      if(iter==i10) arr(ELEM(1,i)) = real(10*i, kind=REAL64)
 #endif
     enddo
     !$omp end parallel do
@@ -127,10 +141,24 @@ contains
     real(kind=REAL64), dimension(:,:), allocatable, intent(inout) :: arr
     integer, intent(in) :: i
 
-    if(i*sin(real(i)) - arr(1,4000) == 0.1234) then
-      arr(:,:) = arr(:,:) * 1.0
+    if(i*sin(real(i)) - arr(ELEM(1,4000)) == 0.1234) then
+      arr(ELEM(:,:)) = arr(ELEM(:,:)) * 1.1233532
+#ifdef COL_MAJ
       print *, arr(1,int(arr(1,3000)/arr(1,2000)))
+#else
+      print *, arr(int(arr(3000,1)/arr(2000,1)),1)
+#endif
     endif
+
+#ifdef DEBUG
+    if (i==1) then
+#ifdef COL_MAJ
+    print *, arr(1,int(arr(1,3000)/arr(1,2000)))
+#else
+    print *, arr(int(arr(3000,1)/arr(2000,1)),1)
+#endif
+    endif
+#endif
 
   end subroutine dummy_sub
 end program test
--- test_case.sh
+++ test_case.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 script=./do_case.F90
+version_list=${1:-A B C D E X}
+rc_list=${2:-R C}
 
-export OMP_NUM_THREADS=4
-export FFLAGS="-O2 -fopenmp -ftree-vectorize -march=znver1 -mtune=generic"
-for version in A B C D E
+echo "md5sum: $(md5sum ${script})"
+
+export OMP_NUM_THREADS=${OMP_NUM_THREADS:-$(nproc)}
+FFLAGS=${FFLAGS:- -O2 -fopenmp -ftree-vectorize -march=znver1 -mtune=generic}
+for rc in $(echo ${rc_list} | tr '[:upper:]' '[:lower:]')
 do
-  echo $version
-  gfortran ${FFLAGS} -Dversion${version} ${script} && time ./a.out
-  gfortran ${FFLAGS} -Dversion${version} -S ${script} && mv $(basename ${script} .F90){,_${version}}.s
-  echo ""; echo "";
+  echo "R/C: ${rc}"
+  if [ ${rc} == "r" ]; then
+    DFLAGS="-DROW_MAJ"
+  else
+    DFLAGS="-DCOL_MAJ"
+  fi
+  TMP_FFLAGS="${FFLAGS} ${DFLAGS}"
+  for version in ${version_list}
+  do
+    echo $version
+    echo gfortran ${TMP_FFLAGS} -Dversion${version} ${script}
+    gfortran ${TMP_FFLAGS} -Dversion${version} ${script} && time ./a.out
+    gfortran ${TMP_FFLAGS} -Dversion${version} -S ${script} && mv $(basename ${script} .F90){,_${version}_${rc}}.s
+    echo ""; echo "";
+  done
 done
