b4018f6a26ff9442c062a2a84669cb8ccdbfe792
Thomas Hayward-Schneider <thomas.hayward@ipp.mpg.de>
2020-12-27 00:05:21 +0100

    repeated RHS terms preproc, F&G (functions) added

--- do_case.F90
+++ do_case.F90
@@ -17,6 +17,8 @@
 #define NP_DIM 1
 #endif
 
+#define _RHS_(x) real(x*i, kind=REAL64)
+
 program test
   use, intrinsic :: iso_fortran_env, only : REAL32, REAL64
   implicit none
@@ -33,18 +35,29 @@ program test
 #endif
 
   do i=1,n
-    call test_sub(array, mod(i, nvars)+1, i1,i2,i3,i4,i5,i6,i7,i8,i9,i10)
+    call test_sub(array, mod(i, nvars)+1 &
+#ifdef versionE
+    , i1,i2,i3,i4,i5,i6,i7,i8,i9,i10 &
+#endif
+    )
     call dummy_sub(array, i) ! dummy_sub prevents the compiler from rewriting test_sub
   enddo
 
 contains
 
-  subroutine test_sub(arr, iter, i1,i2,i3,i4,i5,i6,i7,i8,i9,i10)
+  subroutine test_sub(arr, iter &
+#ifdef versionE
+    , i1,i2,i3,i4,i5,i6,i7,i8,i9,i10 &
+#endif
+    )
     use, intrinsic :: iso_fortran_env, only : REAL32, REAL64
     implicit none
 
     real(kind=REAL64), dimension(:,:), allocatable, intent(inout) :: arr
-    integer, intent(in) :: iter, i1,i2,i3,i4,i5,i6,i7,i8,i9,i10
+    integer, intent(in) :: iter
+#ifdef versionE
+    integer, intent(in) :: i1,i2,i3,i4,i5,i6,i7,i8,i9,i10
+#endif
     integer :: i, imax
 
     imax = ubound(arr, NP_DIM)
@@ -52,83 +65,127 @@ contains
     do i=1, imax
       ! do lots of stuff, some of which the below expressions should depend on
 #ifdef versionA
-      arr(ELEM(1,i)) = real(i, kind=REAL64)
-      arr(ELEM(2,i)) = real(2*i, kind=REAL64)
-      arr(ELEM(3,i)) = real(3*i, kind=REAL64)
-      arr(ELEM(4,i)) = real(4*i, kind=REAL64)
-      arr(ELEM(5,i)) = real(5*i, kind=REAL64)
-      arr(ELEM(6,i)) = real(6*i, kind=REAL64)
-      arr(ELEM(7,i)) = real(7*i, kind=REAL64)
-      arr(ELEM(8,i)) = real(8*i, kind=REAL64)
-      arr(ELEM(9,i)) = real(9*i, kind=REAL64)
-      arr(ELEM(10,i)) = real(10*i, kind=REAL64)
+      arr(ELEM(1,i)) = _RHS_(1)
+      arr(ELEM(2,i)) = _RHS_(2)
+      arr(ELEM(3,i)) = _RHS_(3)
+      arr(ELEM(4,i)) = _RHS_(4)
+      arr(ELEM(5,i)) = _RHS_(5)
+      arr(ELEM(6,i)) = _RHS_(6)
+      arr(ELEM(7,i)) = _RHS_(7)
+      arr(ELEM(8,i)) = _RHS_(8)
+      arr(ELEM(9,i)) = _RHS_(9)
+      arr(ELEM(10,i)) = _RHS_(1)
 #elif defined(versionB)
       select case(iter)
       case(1)
-        arr(ELEM(1,i)) = real(i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(1)
       case(2)
-        arr(ELEM(1,i)) = real(2*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(2)
       case(3)
-        arr(ELEM(1,i)) = real(3*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(3)
       case(4)
-        arr(ELEM(1,i)) = real(4*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(4)
       case(5)
-        arr(ELEM(1,i)) = real(5*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(5)
       case(6)
-        arr(ELEM(1,i)) = real(6*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(6)
       case(7)
-        arr(ELEM(1,i)) = real(7*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(7)
       case(8)
-        arr(ELEM(1,i)) = real(8*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(8)
       case(9)
-        arr(ELEM(1,i)) = real(9*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(9)
       case(10)
-        arr(ELEM(1,i)) = real(10*i, kind=REAL64)
+        arr(ELEM(1,i)) = _RHS_(1)
       end select
 #elif defined(versionC)
-      if(iter==1)  arr(ELEM(1,i)) = real(i, kind=REAL64)
-      if(iter==2)  arr(ELEM(1,i)) = real(2*i, kind=REAL64)
-      if(iter==3)  arr(ELEM(1,i)) = real(3*i, kind=REAL64)
-      if(iter==4)  arr(ELEM(1,i)) = real(4*i, kind=REAL64)
-      if(iter==5)  arr(ELEM(1,i)) = real(5*i, kind=REAL64)
-      if(iter==6)  arr(ELEM(1,i)) = real(6*i, kind=REAL64)
-      if(iter==7)  arr(ELEM(1,i)) = real(7*i, kind=REAL64)
-      if(iter==8)  arr(ELEM(1,i)) = real(8*i, kind=REAL64)
-      if(iter==9)  arr(ELEM(1,i)) = real(9*i, kind=REAL64)
-      if(iter==10) arr(ELEM(1,i)) = real(10*i, kind=REAL64)
+      if(iter==1)  arr(ELEM(1,i)) = _RHS_(1)
+      if(iter==2)  arr(ELEM(1,i)) = _RHS_(2)
+      if(iter==3)  arr(ELEM(1,i)) = _RHS_(3)
+      if(iter==4)  arr(ELEM(1,i)) = _RHS_(4)
+      if(iter==5)  arr(ELEM(1,i)) = _RHS_(5)
+      if(iter==6)  arr(ELEM(1,i)) = _RHS_(6)
+      if(iter==7)  arr(ELEM(1,i)) = _RHS_(7)
+      if(iter==8)  arr(ELEM(1,i)) = _RHS_(8)
+      if(iter==9)  arr(ELEM(1,i)) = _RHS_(9)
+      if(iter==10) arr(ELEM(1,i)) = _RHS_(1)
 #elif defined(versionD)
       if(iter==1) then
-         arr(ELEM(1,i)) = real(i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(1)
       elseif(iter==2) then
-         arr(ELEM(1,i)) = real(2*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(2)
       elseif(iter==3) then
-         arr(ELEM(1,i)) = real(3*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(3)
       elseif(iter==4) then
-         arr(ELEM(1,i)) = real(4*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(4)
       elseif(iter==5) then
-         arr(ELEM(1,i)) = real(5*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(5)
       elseif(iter==6) then
-         arr(ELEM(1,i)) = real(6*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(6)
       elseif(iter==7) then
-         arr(ELEM(1,i)) = real(7*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(7)
       elseif(iter==8) then
-         arr(ELEM(1,i)) = real(8*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(8)
       elseif(iter==9) then
-         arr(ELEM(1,i)) = real(9*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(9)
       elseif(iter==10) then
-         arr(ELEM(1,i)) = real(10*i, kind=REAL64)
+         arr(ELEM(1,i)) = _RHS_(1)
       endif
 #elif defined(versionE)
-      if(iter==i1)  arr(ELEM(1,i)) = real(i, kind=REAL64)
-      if(iter==i2)  arr(ELEM(1,i)) = real(2*i, kind=REAL64)
-      if(iter==i3)  arr(ELEM(1,i)) = real(3*i, kind=REAL64)
-      if(iter==i4)  arr(ELEM(1,i)) = real(4*i, kind=REAL64)
-      if(iter==i5)  arr(ELEM(1,i)) = real(5*i, kind=REAL64)
-      if(iter==i6)  arr(ELEM(1,i)) = real(6*i, kind=REAL64)
-      if(iter==i7)  arr(ELEM(1,i)) = real(7*i, kind=REAL64)
-      if(iter==i8)  arr(ELEM(1,i)) = real(8*i, kind=REAL64)
-      if(iter==i9)  arr(ELEM(1,i)) = real(9*i, kind=REAL64)
-      if(iter==i10) arr(ELEM(1,i)) = real(10*i, kind=REAL64)
+      if(iter==i1)  arr(ELEM(1,i)) = _RHS_(1)
+      if(iter==i2)  arr(ELEM(1,i)) = _RHS_(2)
+      if(iter==i3)  arr(ELEM(1,i)) = _RHS_(3)
+      if(iter==i4)  arr(ELEM(1,i)) = _RHS_(4)
+      if(iter==i5)  arr(ELEM(1,i)) = _RHS_(5)
+      if(iter==i6)  arr(ELEM(1,i)) = _RHS_(6)
+      if(iter==i7)  arr(ELEM(1,i)) = _RHS_(7)
+      if(iter==i8)  arr(ELEM(1,i)) = _RHS_(8)
+      if(iter==i9)  arr(ELEM(1,i)) = _RHS_(9)
+      if(iter==i10) arr(ELEM(1,i)) = _RHS_(1)
+#elif defined(versionF)
+      if(iter==1) then
+        call set_elem(arr, i, 1, _RHS_(1))
+      elseif(iter==2) then
+        call set_elem(arr, i, 1, _RHS_(2))
+      elseif(iter==3) then
+        call set_elem(arr, i, 1, _RHS_(3))
+      elseif(iter==4) then
+        call set_elem(arr, i, 1, _RHS_(4))
+      elseif(iter==5) then
+        call set_elem(arr, i, 1, _RHS_(5))
+      elseif(iter==6) then
+        call set_elem(arr, i, 1, _RHS_(6))
+      elseif(iter==7) then
+        call set_elem(arr, i, 1, _RHS_(7))
+      elseif(iter==8) then
+        call set_elem(arr, i, 1, _RHS_(8))
+      elseif(iter==9) then
+        call set_elem(arr, i, 1, _RHS_(9))
+      elseif(iter==10) then
+        call set_elem(arr, i, 1, _RHS_(10))
+      endif
+#elif defined(versionG)
+      if(iter==1) then
+         call set_elem_val(arr, i, 1, _RHS_(1))
+      elseif(iter==2) then
+         call set_elem_val(arr, i, 1, _RHS_(2))
+      elseif(iter==3) then
+         call set_elem_val(arr, i, 1, _RHS_(3))
+      elseif(iter==4) then
+         call set_elem_val(arr, i, 1, _RHS_(4))
+      elseif(iter==5) then
+         call set_elem_val(arr, i, 1, _RHS_(5))
+      elseif(iter==6) then
+         call set_elem_val(arr, i, 1, _RHS_(6))
+      elseif(iter==7) then
+         call set_elem_val(arr, i, 1, _RHS_(7))
+      elseif(iter==8) then
+         call set_elem_val(arr, i, 1, _RHS_(8))
+      elseif(iter==9) then
+         call set_elem_val(arr, i, 1, _RHS_(9))
+      elseif(iter==10) then
+         call set_elem_val(arr, i, 1, _RHS_(10))
+      endif
 #endif
     enddo
     !$omp end parallel do
@@ -153,12 +210,32 @@ contains
 #ifdef DEBUG
     if (i==1) then
 #ifdef COL_MAJ
-    print *, arr(1,int(arr(1,3000)/arr(1,2000)))
+    print *, arr(1,max(int(arr(1,3000)/arr(1,2000)),100000))
 #else
-    print *, arr(int(arr(3000,1)/arr(2000,1)),1)
+    print *, arr(max(int(arr(3000,1)/arr(2000,1)),100000),1)
 #endif
     endif
 #endif
 
   end subroutine dummy_sub
+
+  subroutine set_elem(arr, ip, col, val)
+    use, intrinsic :: iso_fortran_env, only : REAL32, REAL64
+    implicit none
+    real(kind=REAL64), dimension(:,:), allocatable, intent(inout) :: arr
+    integer, intent(in) :: ip, col
+    real(kind=REAL64), intent(in) :: val
+
+    arr(ELEM(col,ip)) = val
+  end subroutine set_elem
+
+  subroutine set_elem_val(arr, ip, col, val)
+    use, intrinsic :: iso_fortran_env, only : REAL32, REAL64
+    implicit none
+    real(kind=REAL64), dimension(:,:), allocatable, intent(inout) :: arr
+    integer, intent(in), value :: ip, col
+    real(kind=REAL64), intent(in), value :: val
+
+    arr(ELEM(col,ip)) = val
+  end subroutine set_elem_val
 end program test
--- test_case.sh
+++ test_case.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 script=./do_case.F90
-version_list=${1:-A B C D E X}
+version_list=${1:-A B C D E F G X}
 rc_list=${2:-R C}
 
 echo "md5sum: $(md5sum ${script})"
 
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:-$(nproc)}
-FFLAGS=${FFLAGS:- -O2 -fopenmp -ftree-vectorize -march=znver1 -mtune=generic}
+FFLAGS=${FFLAGS:- -O2 -fopenmp -ftree-vectorize -march=znver1 -mtune=generic -g ${DFLAGS}}
 for rc in $(echo ${rc_list} | tr '[:upper:]' '[:lower:]')
 do
   echo "R/C: ${rc}"
   if [ ${rc} == "r" ]; then
-    DFLAGS="-DROW_MAJ"
+    DFLAGS_RC="-DROW_MAJ"
   else
-    DFLAGS="-DCOL_MAJ"
+    DFLAGS_RC="-DCOL_MAJ"
   fi
-  TMP_FFLAGS="${FFLAGS} ${DFLAGS}"
+  TMP_FFLAGS="${FFLAGS} ${DFLAGS_RC}"
   for version in ${version_list}
   do
-    echo $version
+    echo $version ${rc}
     echo gfortran ${TMP_FFLAGS} -Dversion${version} ${script}
     gfortran ${TMP_FFLAGS} -Dversion${version} ${script} && time ./a.out
     gfortran ${TMP_FFLAGS} -Dversion${version} -S ${script} && mv $(basename ${script} .F90){,_${version}_${rc}}.s
